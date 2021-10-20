db564f05fefcb3cf2820cc485aa07a82c81ebd64
mashad <mashad@student.1337.ma>
2021-10-20 13:00:11 +0100

    Adding some tests to main.

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 /*   By:  <>                                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/16 08:17:35 by                   #+#    #+#             */
-/*   Updated: 2021/10/19 10:11:47 by                  ###   ########.fr       */
+/*   Updated: 2021/10/20 12:06:23 by                  ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,37 @@
 # include <vector>
 
 int 	main() {
-	std::vector<int> a(10, 10);
+	ft::Vector<int> a(10, 22);
 
-	ft::Vector<int> vec(a.begin(), a.end());
+	ft::Vector<int> vec(10, 10);
 
-	std::cout << vec[1] << " : " << vec.size() << std::endl;
-	vec.resize(5);
-	std::cout << vec[9] << " : " << vec.size() << std::endl;
+	ft::Vector<int>::iterator t = a.begin();
+	ft::Vector<int>::iterator att = a.end();
+	for (; t != att ; ++t) {
+		std::cout << *t << " ";
+	}
+	std::cout << std::endl;
 	vec.push_back(200);
-	std::cout << vec[6] << " : " << vec.size() << std::endl;
+	std::cout << "VEC SIZE: " << a.capacity() << " " << "A size: "<< vec.capacity() << std::endl;
+	ft::Vector<int>::iterator it = vec.begin();
+	ft::Vector<int>::iterator at = vec.end();
+	for (; it != at ; ++it) {
+		std::cout << *it << " ";
+	}
+	std::cout << std::endl;
+	vec.assign(a.begin() + 2, a.end());
+	it = vec.begin();
+	at = vec.end();
+	for (; it != at ; ++it) {
+		std::cout << *it << " ";
+	}
+	std::cout << std::endl;
+	ft::Vector<int>::iterator d = vec.erase(vec.end() - 5, vec.end());
+	*d = 100;
+	it = vec.begin();
+	at = vec.end();
+	for (; it != at ; ++it) {
+		std::cout << *it << " ";
+	}
+	std::cout << std::endl;
 }
