0f6907791fb1ef25bbc1d5878b8a747b0ea9391c
mashad <mashad@student.1337.ma>
2021-11-06 08:36:08 +0100

    Implementing red black tree iterator.

--- Map/red_black_tree_iterator.hpp
+++ Map/red_black_tree_iterator.hpp
@@ -5,4 +5,14 @@
 #ifndef FT_CONTAINERS_RED_BLACK_TREE_ITERATOR_HPP
 #define FT_CONTAINERS_RED_BLACK_TREE_ITERATOR_HPP
 
+namespace ft {
+	
+	template <class T>
+	class rbtiterator: ft::biderectional_iterator<T> {
+		public:
+			typedef typename ft::biderectional_iterator<T>::value_type	value_type;
+			typedef typename ft::biderectional_iterator<T>::pointer		pointer;
+			typedef typename ft::biderectional_iterator<T>::
+	};
+}
 #endif //FT_CONTAINERS_RED_BLACK_TREE_ITERATOR_HPP
