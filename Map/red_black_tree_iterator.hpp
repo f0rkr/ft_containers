a49a62b312ae7bff9a41c6e359f77063a332474a
mashad <mashad@student.1337.ma>
2021-11-06 13:08:34 +0100

    Red black tree iterator.

--- Map/red_black_tree_iterator.hpp
+++ Map/red_black_tree_iterator.hpp
@@ -5,14 +5,37 @@
 #ifndef FT_CONTAINERS_RED_BLACK_TREE_ITERATOR_HPP
 #define FT_CONTAINERS_RED_BLACK_TREE_ITERATOR_HPP
 
+# include <iostream>
+
 namespace ft {
 	
 	template <class T>
-	class rbtiterator: ft::biderectional_iterator<T> {
+	class rbTreeIterator: public ft::iterator<std::bidirectional_iterator_tag, T> {
 		public:
-			typedef typename ft::biderectional_iterator<T>::value_type	value_type;
-			typedef typename ft::biderectional_iterator<T>::pointer		pointer;
-			typedef typename ft::biderectional_iterator<T>::
+			typedef typename ft::iterator<std::bidirectional_iterator_tag, T>::iterator_category	iterator_category;
+			typedef typename ft::iterator<std::bidirectional_iterator_tag, T>::value_type			value_type;
+			typedef typename ft::iterator<std::bidirectional_iterator_tag, T>::pointer				pointer;
+			typedef typename ft::iterator<std::bidirectional_iterator_tag, T>::reference			reference;
+			typedef typename ft::iterator<std::bidirectional_iterator_tag, T>::difference_type		difference_type;
+
+			rbTreeIterator() : _ptr(nullptr) {};
+			rbTreeIterator(pointer ptr): _ptr(ptr) {};
+			rbTreeIterator	&operator=(rbTreeIterator const &other) {
+				_ptr = other._ptr;
+				return (*this);
+			}
+
+			reference 	operator*();
+			reference 	operator*() const;
+			pointer		operator->();
+			pointer 	operator->() const;
+
+
+			friend bool		operator==(const rbTreeIterator &lhs, const rbTreeIterator &rhs);
+			friend bool 	operator!=(const rbTreeIterator &lhs, const rbTreeIterator &rhs);
+
+		private:
+			pointer		_ptr;
 	};
 }
 #endif //FT_CONTAINERS_RED_BLACK_TREE_ITERATOR_HPP
